a6d5850022bcecb02c80a4e3ca39d730be151f4b
vitzwitz <b.l.miskovitz@gmail.com>
2017-11-02 14:44:01 -0400

    Working on slideshow

--- TylersWebsite/slideshow.js
+++ TylersWebsite/slideshow.js
@@ -20,22 +20,14 @@ function showSlide
     var i;
     var slides = document.getElementsByClassName('theSlides');
 
-    // go to end
-    if (n<1)
-    {
-      index = slides.length;
-    }
-
-
     // start from beginning
-    if (n>slides.length)
-    {
-      index = 1
-    }
+    if (n>slides.length) {index = 1}
+    // go to end
+    if (n<1) {index = slides.length}
 
     for (i=0; i<slides.length; i++)
     {
       slides[i].style.display = "none";
     }
     slides[index - 1].style.display = "block";
-}
+  }
