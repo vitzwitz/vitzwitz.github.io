6a215cfc8df033d59211986021c4dbf982434136
vitzwitz <b.l.miskovitz@gmail.com>
2017-11-06 03:15:12 -0500

    New navigation bar/other site

---
+++ popup.js
@@ -0,0 +1,13 @@
+
+// temporary
+function popUp() {
+  alert("Website is still in progress")
+}
+
+function openNav() {
+    document.getElementById('nav').style.height = 100%;
+}
+
+function closeNav() {
+    document.getElementById('nav').style.height = 0%;
+}
