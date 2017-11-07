2f8666e0f886cc5a2d45fa2bc91fbc811cbfe586
vitzwitz <b.l.miskovitz@gmail.com>
2017-11-07 12:57:42 -0500

    nav bar update in progress

--- popup.js
+++ popup.js
@@ -4,10 +4,16 @@ function popUp() {
   alert("Website is still in progress")
 }
 
-function openNav() {
-    document.getElementById('nav').style.height = 100%;
-}
+var drpdwn = document.getElementsByClassName("dropbtt");
+var i;
 
-function closeNav() {
-    document.getElementById('nav').style.height = 0%;
+for (i=0; i<drpdwn.length; i++) {
+  drpdwn[i].onclick = function() {
+    var dropdown_menu = this.nextElementSibling;
+    if (dropdown_menu.style.maxHeight) {
+      dropdown_menu.style.maxHeight = null;
+    } else {
+      dropdown_menu.style.maxHeight = dropdown_menu.scrollHeight + "px";
+    }
+  }
 }
